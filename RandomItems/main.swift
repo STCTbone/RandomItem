e3861303e83995de51adbe00acd2ffc24841dfc4
Matthew Rieger <matthewrieger@Matthews-MacBook-Pro.local>
2014-06-24 19:45:59 -0500

    Initial Commit

---
+++ RandomItems/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RandomItems
+//
+//  Created by Matthew Rieger on 6/24/14.
+//  Copyright (c) 2014 Matthew Rieger. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
